fa2570227f5fd8536fb5c7b92aa12f69bb9075c6
Frederic Laurent <frederic.laurent@gmail.com>
2020-06-19 10:26:02 +0200

    deploiement mac app

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 buildOptions = dict(packages = ["multiprocessing"], excludes = ["tkinter"])
-
+#python setup.py bdist_mac --iconfile=assets/banana.icns --custom-info-plist=Info.plist
+macOptions = {"iconfile":"assets/banana.icns", "custom_info_plist":"Info.plist"}
+dmgOptions = {"volume_label": "banana", "applications_shortcut": True}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -23,4 +25,5 @@ setup(  name = "banana",
         version = "0.1",
         description = "My Banana!",
         options = {"build_exe": buildOptions},
+        #, "bdist_mac":macOptions, "bdist_dmg":dmgOptions},
         executables = executables)
