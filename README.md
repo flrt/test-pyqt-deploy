2a9a90b4d24bed2517a6763ef552338823cc9759
Frederic Laurent <flt@opikanoba.org>
2020-06-20 12:05:16 +0200

    linux dist

---
+++ README.md
@@ -0,0 +1,18 @@
+Test the deployment of a simple app on
+
+- windows
+- macos
+- linux
+
+Tested Tool
+
+-pyinstaler
+-cxFreeze
+
+
+
+# cxFreeze
+python setup.py build
+sudo cp app.desktop /usr/share/applications/banana.desktop
+sudo cp -r venv/lib/banana-1.0/ /usr/share/banana
+sudo cp assets/banana.ico /usr/share/pixmaps/banana.ico
--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import sys
 from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need
-# fine tuning.
-buildOptions = dict(packages = ["multiprocessing"], excludes = ["tkinter"])
-#python setup.py bdist_mac --iconfile=assets/banana.icns --custom-info-plist=Info.plist
-macOptions = {"iconfile":"assets/banana.icns", "custom_info_plist":"Info.plist"}
+APPNAME = 'banana'
+APPDESCR = 'banana test'
+MAIN = 'main.py'
+__version__ = '1.0'
+
+buildOptions = dict(packages=["multiprocessing"], excludes=["tkinter"])
+
+# On Mac OS
+# python setup.py bdist_mac --iconfile=assets/banana.icns --custom-info-plist=Info.plist
+# in setup , "bdist_mac":macOptions, "bdist_dmg":dmgOptions},
+macOptions = {"iconfile": "assets/banana.icns", "custom_info_plist": "Info.plist"}
 dmgOptions = {"volume_label": "banana", "applications_shortcut": True}
 
 # GUI applications require a different base on Windows (the default is for a
@@ -14,16 +20,12 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-
 executables = [
-    Executable('main.py', base=base, targetName = 'banana', icon='assets/banana.ico')
+    Executable(MAIN, base=base, targetName=APPNAME, icon='assets/banana.ico')
 ]
 
-
-
-setup(  name = "banana",
-        version = "0.1",
-        description = "My Banana!",
-        options = {"build_exe": buildOptions},
-        #, "bdist_mac":macOptions, "bdist_dmg":dmgOptions},
-        executables = executables)
+setup(name=APPNAME,
+      version=__version__,
+      description=APPDESCR,
+      options={"build_exe": buildOptions},
+      executables=executables)
